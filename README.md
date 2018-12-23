c64ff398128bfdaca6de62110efe041b2dd72985
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2018-12-23 17:58:32 +0100

    First big commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Swift Scripting Bridge
+
+## Description
+
+Swift libraries for using scripting bridge. Thank's to https://github.com/tingraldi/SwiftScripting . I'am only a copy-paste man ;)
+
+## Usage
+
+Note that the sbhc.py script leverages the Python bindings for libclang. You must install these bindings before running the script. You'll also need to install the Xcode command line tools 
+
+First, you need install python 'clang' module.
+
+```bash
+$sudo easy_install clang
+```
+
+Now you have to run this shell script, for building and installing scripting build frameworks in /Library/Frameworks
+
+```bash
+./install.sh
+```
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/rvillamil/iTunesQuickRatingBar/tags).
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# Copyright (C) Rodrigo Villamil Perez 2018
+# Fichero: create-definitions.sh
+# Autor: Rodrigo
+# Fecha: 23/12/18
+#
+
+# CONSTANTS
+TOOLS_DIR=`pwd`
+PROJECTS=`/bin/ls -d Frameworks/*`
+FRAMEWORKS_DIR="Frameworks"
+INSTALL_FRAMEWORKS_DIR="/Library/Frameworks"
+
+ITUNES_PROJECT_DIR="${FRAMEWORKS_DIR}/iTunes/iTunesScripting/"
+
+#
+# ${1}: app name
+#
+function createHeaderFiles() {
+    app_name="${1}"
+    output="${FRAMEWORKS_DIR}/${app_name}/${app_name}Scripting"
+
+    echo "* Creating objective-C headers for app '${app_name}' in '${output}' directory ..."
+    cd ${output} > /dev/null 2>&1
+    sdef /Applications/${app_name}.app > ${app_name}.sdef
+    sdp -fh --basename ${app_name} ${app_name}.sdef
+    cd - > /dev/null 2>&1
+
+    return 0
+}
+
+#
+# ${1}: app name
+#
+function createHeaderSwiftFiles() {  
+    app_name="${1}"
+    output="${FRAMEWORKS_DIR}/${app_name}/${app_name}Scripting"
+    echo "* Creating swift headers with for app '${app_name}' in '${output}' directory ..."
+    cd ${output} > /dev/null 2>&1
+    python ${TOOLS_DIR}/sbhc.py ${app_name}.h &&
+    python ${TOOLS_DIR}/sbsc.py ${app_name}.sdef
+    cd - > /dev/null 2>&1
+  
+    return 0
+}
+
+#
+# ${1}: app name
+#
+function build () {
+    app_name="${1}"
+   
+    echo "* Building project '${app_name}' with xcodebuild tools ..'"
+    cd "${FRAMEWORKS_DIR}/${app_name}" > /dev/null 2>&1
+    rm -rf build
+    xcodebuild
+    res=${?}
+    cd - > /dev/null 2>&1
+    
+    return ${res}
+}
+
+#
+# ${1}: app name
+#
+function install () {
+    app_name="${1}"
+   
+    echo "* Install project '${app_name}' in ${INSTALL_FRAMEWORKS_DIR}'"
+    cd "${FRAMEWORKS_DIR}/${app_name}" > /dev/null 2>&1
+    sudo mv "build/Release/${app_name}Scripting.framework" ${INSTALL_FRAMEWORKS_DIR} 
+    res=${?}
+    cd - > /dev/null 2>&1
+    
+    return ${res}
+}
+
+#
+# -- Main
+#
+createHeaderFiles "iTunes" &&
+createHeaderSwiftFiles "iTunes" &&
+build "iTunes" &&
+install "iTunes"
